f32bdf44f0435909b59a92cb0ce34e6a13f3e11d
anilmeister <anilkapikiran@hotmail.com>
2024-05-08 00:29:31 +0300

    Dataset implementation and exploratory data analysis.

---
+++ load_data.py
@@ -0,0 +1,7 @@
+# Importing Libraries
+import pandas as pd
+
+# Loading data files
+train_data = pd.read_csv("data/train.csv")
+test_data = pd.read_csv("data/test.csv")
+original_data = pd.read_csv('data/original_data.csv')
